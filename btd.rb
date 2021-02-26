9591b7bec844035789bdbd49f09947dc9d623b44
Josh Garth <jpgarth+git@gmail.com>
2021-02-26 12:43:02 +0100

    Add savings plan calculation

--- btd.rb
+++ btd.rb
@@ -2,8 +2,11 @@
 
 require 'csv'
 require 'time'
+require 'date'
 require './time_series.rb'
 require 'byebug'
+
+START_TIME = Time.parse('2020-01-01')
 
 def execute_on_timeseries(&block)
   db = TimeSeries.new
@@ -12,7 +15,7 @@ def execute_on_timeseries(&block)
   CSV.foreach('btcusd.csv', headers: true, header_converters: :symbol) do |row|
     time = Time.parse(row[:date])
 
-    next if time < Time.parse('2020-01-01') # ignore data before 2020
+    next if time < START_TIME # ignore data before 2020
 
     most_recent_price = row[:high].to_i
     db << TimeSeries::DataPoint.new(time, most_recent_price)
@@ -45,25 +48,89 @@ def backtest(p=0.9, q=1.1)
 
   puts
   puts "\e[32mRESULT $#{(btc * last_price + usd).round(2)}\e[0m"
-
 end
+
+def diamond_hands(p=0.9, usd_per_month=100)
+  usd = usd_per_month
+  last_payin = START_TIME
+  next_payin = (last_payin.to_date >> 1).to_time
+  btc = 0
+
+  last_price = execute_on_timeseries do |price, recent, time|
+    if time >= next_payin
+      usd += usd_per_month
+      last_payin = time
+      next_payin = (last_payin.to_date >> 1).to_time
+      puts "#{time} ADD BUDGET $#{usd_per_month}. BUDGET NOW $#{usd}"
+    end
+
+    if usd > 0 && price < recent.max * p
+      btc += usd.fdiv(price)
+      puts "#{time} BUY ฿#{btc.round(5)} worth $#{usd.round(2)} @ $#{price}"
+      usd = 0
+    end
+  end
+
+  puts
+  puts "\e[32mRESULT $#{(btc * last_price + usd).round(2)}\e[0m"
+end
+
+puts "Trading with fixed amount"
+puts "-------------------------"
+puts
 
 puts "\e[4mTest 1: Buy when 2% down, sell when 2% up\e[24m"
 puts
 backtest(0.98, 1.02)
 puts
+
 puts "\e[4mTest 2: Buy when 5% down, sell when 5% up\e[24m"
 puts
 backtest(0.95, 1.05)
 puts
+
 puts "\e[4mTest 3: Buy when 10% down, sell when 10% up\e[24m"
 puts
 backtest(0.9, 1.1)
 puts
+
 puts "\e[4mTest 4: Buy when 5% down, sell when 10% up\e[24m"
 puts
 backtest(0.95, 1.1)
 puts
+
 puts "\e[4mTest 5: HODL\e[24m"
 puts
 backtest(Float::INFINITY, Float::INFINITY)
+puts
+
+puts
+puts
+puts "Savings plan"
+puts "------------"
+puts
+
+puts "\e[4mTest 6: Buy 100$ / month, regardless of price\e[24m"
+puts
+diamond_hands(Float::INFINITY, 100)
+puts
+
+puts "\e[4mTest 7: Buy 100$ / month, only if 1% down\e[24m"
+puts
+diamond_hands(0.99, 100)
+puts
+
+puts "\e[4mTest 8: Buy 100$ / month, only if 5% down\e[24m"
+puts
+diamond_hands(0.95, 100)
+puts
+
+puts "\e[4mTest 9: Buy 100$ / month, only if 10% down\e[24m"
+puts
+diamond_hands(0.90, 100)
+puts
+
+puts "\e[4mTest 10: Buy 100$ / month, only if 15% down\e[24m"
+puts
+diamond_hands(0.85, 100)
+puts
